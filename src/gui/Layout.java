b3bebadca56388bfbb9752bf784633f3bf1783a3
Nate3447 <nathaniel.sierras@gmail.com>
2016-04-08 03:44:24 +0800

    ActionListener Update.

--- src/gui/Layout.java
+++ src/gui/Layout.java
@@ -7,8 +7,26 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+import transaction.IsolationLevel;
+import transaction.Transaction;
+import transaction.TransactionRunnable;
+
 
 public class Layout extends JFrame{
+	
+	private JRadioButton radioLocal;
+    private JRadioButton radioGlobal;
+    private JRadioButton radioReadUC;
+    private JRadioButton radioReadC;
+    private JRadioButton radioRepeatRead;
+    private JRadioButton radioSerial;
+    private JRadioButton radioCase1;
+    private JRadioButton radioCase2;
+    private JRadioButton radioCase3;
+    
+    private JTextField txt_id;
+    private JComboBox txt_province;
+    private JTextField txt_age;
 	
 	 public static void main(String[] args) {
 		Layout frame = new Layout();
@@ -40,23 +58,24 @@ public class Layout extends JFrame{
 	     JLabel lbl_8 = new JLabel("Isolation Level");
 	     JLabel lbl_9 = new JLabel("Case");
 	     
-	     JTextField txt_id = new JTextField(5);
-	     JTextField txt_province = new JTextField(10);
-	     JTextField txt_age = new JTextField(3);
-	     
 	     JTextArea resultsArea1 = new JTextArea("Local / Global", 10, 10);
 	     JTextArea resultsArea2 = new JTextArea("Local 2", 10, 10);
 	     JTextArea resultsArea3 = new JTextArea("Local 3", 10, 10);
 	     
-	     JRadioButton radioLocal = new JRadioButton("Local");
-	     JRadioButton radioGlobal = new JRadioButton("Global");
-	     JRadioButton radioReadUC = new JRadioButton("Read Uncommitted");
-	     JRadioButton radioReadC = new JRadioButton("Read Committed");
-	     JRadioButton radioRepeatRead = new JRadioButton("Repeatable Read");
-	     JRadioButton radioSerial = new JRadioButton("Serializable");
-	     JRadioButton radioCase1 = new JRadioButton("Read - Read");
-	     JRadioButton radioCase2 = new JRadioButton("Write - Read");
-	     JRadioButton radioCase3 = new JRadioButton("Write - Write");
+	     txt_id = new JTextField(5);
+	     String[] options = { "palawan", "marinduque"};
+	     txt_province = new JComboBox(options);
+	     txt_age = new JTextField(3);
+	     
+	     radioLocal = new JRadioButton("Local");
+	     radioGlobal = new JRadioButton("Global");
+	     radioReadUC = new JRadioButton("Read Uncommitted");
+	     radioReadC = new JRadioButton("Read Committed");
+	     radioRepeatRead = new JRadioButton("Repeatable Read");
+	     radioSerial = new JRadioButton("Serializable");
+	     radioCase1 = new JRadioButton("Read - Read");
+	     radioCase2 = new JRadioButton("Write - Read");
+	     radioCase3 = new JRadioButton("Write - Write");
 	     
 	     ButtonGroup concurrencyGroup = new ButtonGroup();
 	     ButtonGroup isolationGroup = new ButtonGroup();
@@ -71,7 +90,7 @@ public class Layout extends JFrame{
 	     caseGroup.add(radioCase1);
 	     caseGroup.add(radioCase2);
 	     caseGroup.add(radioCase3);
-	   
+
 	     leftPanel.add(br);
 	     leftPanel.add(lbl_7);
 	     leftPanel.add(radioLocal);
@@ -121,11 +140,90 @@ public class Layout extends JFrame{
 		 
 		 btn_exec.addActionListener(new ActionListener(){
 	            public void actionPerformed(ActionEvent e) {
+	            	// DELETE AFTER FIXING
 	            	resultsArea1.setText("Test 1");
 	            	resultsArea2.setText("Test 2");
 	            	resultsArea3.setText("Test 3");
+	            	// DELETE AFTER FIXING
+	            	
+	            	if((!radioLocal.isSelected() && !radioGlobal.isSelected()) || 
+	            		(!radioReadUC.isSelected() && !radioReadC.isSelected() && !radioRepeatRead.isSelected() && !radioSerial.isSelected()) ||
+	            		(!radioCase1.isSelected() && !radioCase2.isSelected() && !radioCase3.isSelected())) {
+	            		;
+	            	}
+	            	else {
+	            		
+	            	}
+	            	
 	            }
 	        });
 	    }
+	 
+	 public void localCase1() {
+		 // Thread 1 and thread 2 read		 
+		 Transaction read = new Transaction(getIsolationLevel(), getReadStatement());
+		 TransactionRunnable thread1 = new TransactionRunnable(read);
+		 TransactionRunnable thread2 = new TransactionRunnable(read);
+		 thread1.run();
+		 thread2.run();
+	 }
+	 
+	 public void localCase2() {
+		 // Thread 1 writes. Thread 2 reads.
+		 String statement;
+		 Transaction read = new Transaction(getIsolationLevel(), getReadStatement());
+		 Transaction write = new Transaction(getIsolationLevel(), getWriteStatement());
+		 TransactionRunnable thread1 = new TransactionRunnable(write);
+		 TransactionRunnable thread2 = new TransactionRunnable(read);
+		 thread1.run();
+		 thread2.run();
+	 }
 
+	 public void localCase3() {
+		 // Thread 1 writes. Thread 2 writes.
+		 String statement;
+		 
+		 
+	 }
+	 
+	 public void globalCase1() {
+		 
+	 }
+	 
+	 public void globalCase2() {
+		 
+	 }
+	 
+	 public void globalCase3() {
+		 
+	 }
+	 
+	 public String getReadStatement() {
+		 return "SELECT id, province, age FROM hpq_mem WHERE id = " 
+				 + txt_id.getText() + " AND " 
+				 + "province = " + (String)txt_province.getSelectedItem() + ";";
+	 }
+	 
+	 public String getWriteStatement() {
+		 return "UPDATE hpq_mem SET age = " + txt_age.getText() 
+		 	+ " WHERE id = " + txt_id.getText() + " AND " 
+		 	+ "province = " + (String)txt_province.getSelectedItem() + "; " 
+		 	+ "SELECT id, province, age FROM hpq_mem WHERE id = "
+		 	+ txt_id.getText() + " AND "
+		 	+ "province = " + (String)txt_province.getSelectedItem() + ";";
+	 }
+	 
+	 public IsolationLevel getIsolationLevel() {
+		 if (radioReadUC.isSelected()) {
+			 return IsolationLevel.READ_UNCOMMITTED;
+		 } else if (radioReadC.isSelected()) {
+			 return IsolationLevel.READ_COMMITTED;
+		 } else if (radioRepeatRead.isSelected()) {
+			 return IsolationLevel.READ_REPEATABLE;
+		 } else if (radioSerial.isSelected()) {
+			 return IsolationLevel.SERIALIZABLE;
+		 } else {
+			 return IsolationLevel.NONE;
+		 }
+	 }
 }
