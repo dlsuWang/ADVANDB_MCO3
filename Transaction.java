2dc0bdc5d47f28be57d3dc3d5617a1a66aa9004f
Nate3447 <nathaniel.sierras@gmail.com>
2016-04-07 17:30:43 +0800

    Updated IsolationLevel and TransactionRunnable.
    Will be working on TransactionRunnable.

--- Transaction.java
+++ Transaction.java
@@ -4,6 +4,11 @@ public class Transaction {
 	private IsolationLevel level
 	private String sql; 
 	
+	public Transaction(String sql) {
+		level = NONE;
+		this.sql = sql;
+	}
+	
 	public Transaction(IsolationLevel level, String sql) {
 		this.level = level;
 		this.sql = sql;
@@ -11,5 +16,9 @@ public class Transaction {
 	
 	public IsolationLevel getIsolationLevel() {
 		return level;
+	}
+	
+	public String getSQL() {
+		return sql;
 	}
 }
--- TransactionRunnable.java
+++ TransactionRunnable.java
@@ -3,12 +3,13 @@ public class TransactionRunnable implements Runnable {
 
 	// EACH NODE IS ONE THREAD
 	
-	private DBConnector connector;
-	private String transaction;
+	private DBConnector connection;
+	private Transaction transaction;
 	
 	public TransactionRunnable(String transaction) {
 		this.transaction = transaction;
 		
+		DBConnector.queryDB(transaction.getSQL());
 		// send to database
 	}
 	
