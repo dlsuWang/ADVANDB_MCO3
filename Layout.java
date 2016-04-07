977426cce2396b4ff2ab52ca563cf9a096cc5e2a
Nate3447 <nathaniel.sierras@gmail.com>
2016-04-08 00:17:37 +0800

    Jeno's GUI

--- Layout.java
+++ Layout.java
@@ -1,65 +1,131 @@
+package advandb;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
-import java.awt.event.*;
-import java.awt.*;
 
-public class Layout extends JFrame {
+
+public class Layout extends JFrame{
 	
-    public static void main(String[] args) {
-    	Layout frame = new Layout();
-    	frame.setSize(400,400);
-        frame.GUI();
-    } 
-    
+	 public static void main(String[] args) {
+		Layout frame = new Layout();
+	    frame.setSize(400,600);
+	    frame.setResizable(false);
+	    frame.GUI();
+	    } 
+	 
+	 void GUI() {
+		 Container c = getContentPane();
+		 
+		 JPanel leftPanel = new JPanel();
+		 JPanel rightPanel = new JPanel();
+		 JPanel txtFieldPanel = new JPanel();
+		 
+		 JSplitPane pane = new JSplitPane( JSplitPane.HORIZONTAL_SPLIT, 
+                 leftPanel, rightPanel );
+		 
+		 JButton btn_exec = new JButton("EXECUTE");
+	     
+		 JLabel br = new JLabel(" ");
+	     JLabel lbl_1 = new JLabel("Local 1 / Global");
+	     JLabel lbl_2 = new JLabel("ID: ");
+	     JLabel lbl_3 = new JLabel("Province: ");
+	     JLabel lbl_4 = new JLabel("Age: ");
+	     JLabel lbl_5 = new JLabel("Local 2");
+	     JLabel lbl_6 = new JLabel("Local 3");
+	     JLabel lbl_7 = new JLabel("Concurrency");
+	     JLabel lbl_8 = new JLabel("Isolation Level");
+	     JLabel lbl_9 = new JLabel("Case");
+	     
+	     JTextField txt_id = new JTextField(5);
+	     JTextField txt_province = new JTextField(10);
+	     JTextField txt_age = new JTextField(3);
+	     
+	     JTextArea resultsArea1 = new JTextArea("Local / Global", 10, 10);
+	     JTextArea resultsArea2 = new JTextArea("Local 2", 10, 10);
+	     JTextArea resultsArea3 = new JTextArea("Local 3", 10, 10);
+	     
+	     JRadioButton radioLocal = new JRadioButton("Local");
+	     JRadioButton radioGlobal = new JRadioButton("Global");
+	     JRadioButton radioReadUC = new JRadioButton("Read Uncommitted");
+	     JRadioButton radioReadC = new JRadioButton("Read Committed");
+	     JRadioButton radioRepeatRead = new JRadioButton("Repeatable Read");
+	     JRadioButton radioSerial = new JRadioButton("Serializable");
+	     JRadioButton radioCase1 = new JRadioButton("Read - Read");
+	     JRadioButton radioCase2 = new JRadioButton("Write - Read");
+	     JRadioButton radioCase3 = new JRadioButton("Write - Write");
+	     
+	     ButtonGroup concurrencyGroup = new ButtonGroup();
+	     ButtonGroup isolationGroup = new ButtonGroup();
+	     ButtonGroup caseGroup = new ButtonGroup();
+	     
+	     concurrencyGroup.add(radioLocal);
+	     concurrencyGroup.add(radioGlobal);
+	     isolationGroup.add(radioReadUC);
+	     isolationGroup.add(radioReadC);
+	     isolationGroup.add(radioRepeatRead);
+	     isolationGroup.add(radioSerial);
+	     caseGroup.add(radioCase1);
+	     caseGroup.add(radioCase2);
+	     caseGroup.add(radioCase3);
+	   
+	     leftPanel.add(br);
+	     leftPanel.add(lbl_7);
+	     leftPanel.add(radioLocal);
+	     leftPanel.add(radioGlobal);
+	     leftPanel.add(br);
+	     leftPanel.add(lbl_8);
+	     leftPanel.add(radioReadUC);
+	     leftPanel.add(radioReadC);
+	     leftPanel.add(radioRepeatRead);
+	     leftPanel.add(radioSerial);
+	     leftPanel.add(br);
+	     leftPanel.add(lbl_9);
+	     leftPanel.add(radioCase1);
+	     leftPanel.add(radioCase2);
+	     leftPanel.add(radioCase3);
+	     leftPanel.add(br);
+	     leftPanel.add(btn_exec);
+	     
+	     
+	     rightPanel.add(lbl_1);
+	     rightPanel.add(resultsArea1);
+	 
+	     
+	     txtFieldPanel.add(lbl_2);
+	     txtFieldPanel.add(txt_id);
+	     txtFieldPanel.add(lbl_3);
+	     txtFieldPanel.add(txt_province);
+	     txtFieldPanel.add(lbl_4);
+	     txtFieldPanel.add(txt_age);
+	     
+	     rightPanel.add(txtFieldPanel);
+	     
+	     rightPanel.add(lbl_5);
+	     rightPanel.add(resultsArea2);
+	     rightPanel.add(lbl_6);
+	     rightPanel.add(resultsArea3);
+	   
+	     rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+	     leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+	     c.setLayout(new BorderLayout());
+	     c.add(leftPanel, BorderLayout.WEST);
+	     c.add(rightPanel, BorderLayout.EAST);
+	  
+		 
+		 pack();
+		 setVisible(true);
+		 
+		 btn_exec.addActionListener(new ActionListener(){
+	            public void actionPerformed(ActionEvent e) {
+	            	resultsArea1.setText("Test 1");
+	            	resultsArea2.setText("Test 2");
+	            	resultsArea3.setText("Test 3");
+	            }
+	        });
+	    }
 
-    void GUI() {
-        Container c = getContentPane();
- 
-        JPanel p1 = new JPanel();
-   	 	JButton btn_cs1 = new JButton("Case 1");
-   	 	JButton btn_cs2 = new JButton("Case 2");
-     	JButton btn_cs3 = new JButton("Case 3");
-     	JTextArea queryArea = new JTextArea("Hello", 20, 50);
-     	JTextBox idInput = new JTextBox();
-     	JTextBox ageInput = new JTextBox();
-     
-        p1.setLayout(new BorderLayout());
-       
-        p1.add(queryArea, BorderLayout.NORTH);
- 
-        JPanel p2 = new JPanel();
-        
-        p2.add(btn_cs1);
-        p2.add(btn_cs2);
-        p2.add(btn_cs3);
- 
-        c.setLayout(new BorderLayout());
-        c.add(p1, BorderLayout.CENTER);
-        c.add(p2, BorderLayout.SOUTH);
-       
-        pack();
-        setVisible(true);
-        
-        btn_cs1.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                queryArea.setText("Case 1");
-            }
-        });
-        
-        btn_cs2.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                queryArea.setText("Case 2");
-            }
-        });
-        
-        btn_cs3.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                queryArea.setText("Case 3");
-            }
-        });
-  
-        
-    }
-    
-    
 }
-
